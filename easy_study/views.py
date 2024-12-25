2e975c8da94f07ba60ddd04509ab132ba4d8d67b
manish jat 23 <23f3004152@ds.study.iitm.ac.in>
2024-12-25 12:29:07 +0530

    user db updated

--- easy_study/views.py
+++ easy_study/views.py
@@ -1,5 +1,6 @@
 import requests
 from django.shortcuts import render , HttpResponse, HttpResponseRedirect
+from django.contrib.auth.models import User
 
 from users import models
 # from api import active_companies_dict
@@ -7,9 +8,16 @@ from users import models
 def jatmanis1(request):
     custs = models.Cust.objects.all()
     print(custs)
-    for i in custs:
-        print(i.name, i.username, i.password)
-    return render(request, "jatmanis1.html")
+    user_id = request.user.id 
+    user = User.objects.filter(id =  user_id).first()
+    cust = models.Cust.objects.filter(username = user.username).first()
+    print(user)
+    context = {'cust':cust}
+    # context['cust']= cust
+    # for i in cust:
+    #     print(i.name,i.id, i.username, i.password)
+    print(context)
+    return render(request, "jatmanis1.html",context=context)
 
 def admin(request):
     cust = models.Cust.objects.all()
